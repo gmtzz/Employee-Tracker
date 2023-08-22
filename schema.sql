fd79b51504ac3431a8e0a74a563f6fa8458992a7
Giselle <martinezgiselle5@gmail.com>
2023-08-22 14:43:35 -0400

    added inquirer prompts and switch cases

--- schema.sql
+++ schema.sql
@@ -1,3 +1,11 @@
 -- Create DB --
 DROP DATABASE IF EXISTS employee_db;
-CREATE database employee_db;
+CREATE DATABASE employee_db;
+-- selected DB to use for future commands --
+USE employee_db;
+
+CREATE TABLE department (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    department_name VARCHAR(30) NOT NULL
+
+);
--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // dependencies
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const cfonts = require('cfonts');
 
 // creates connection to sql database
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3001,
     user: 'root',
     password: 'Disney101!',
     database: 'employee_db'
@@ -16,8 +16,76 @@ const connection = mysql.createConnection({
 connection.connect(function(err){
 
     // error if there is an issue when connecting 
-    if (err) throw err;
+    err? console.log(err) : console.log("connected")
 
    
 
 });
+const fontConfig={
+    font: "3d",
+    align: "center",
+    colors:["blue","magenta"],
+    background: "black",
+    letterSpacing: 1,
+    lineHeight: 1,
+    fontSize: "small",
+
+}
+
+function displayLogo(){
+    cfonts.say("Emplo\nyee\nTra\ncker",fontConfig)
+    console.log("-------------------------------------------------------")
+
+}
+function startTracker (){
+    displayLogo()
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "StartingOptions",
+            message: "What would you like to do?",
+            choices: ["view_all_departments",
+            "view_all_roles",
+            "view_all_employees",
+            "add_a_department",
+            "add_a_role",
+            "add_an_employee",
+            "update_employee",
+            "quit"
+        ]
+        }
+    ])
+    .then ((userAnswers)=>{
+        let choices = userAnswers.startingOptions
+        switch(choices){
+            case "view_all_departments": 
+            viewAllDepartments();
+            break;
+            case "view_all_roles": 
+            viewAllRoles();
+            break;
+            case "view_all_employees": 
+            viewAllEmployees();
+            break;
+            case "add_a_department": 
+            addADepartment();
+            break;
+            case "add_a_role": 
+            addARole();
+            break;
+            case "add_an_employee": 
+            addAnEmployee();
+            break;
+            case "update_employee": 
+            updateEmployee();
+            break;
+            case "quit": 
+            quit();
+            break;
+        
+
+        }
+    })
+
+}
+startTracker()
